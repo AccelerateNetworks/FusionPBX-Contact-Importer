e70d54f2ce42b49f78ea25ea25313e19522abf6d
Finn <finn@callpipe.com>
2016-03-03 09:14:10 -0800

    Accept a call UUID, look up the domain and number

--- cnam.php
+++ cnam.php
@@ -9,10 +9,9 @@ require_once __DIR__."/utils.php";
 
 $settings = json_decode(file_get_contents(__DIR__."/settings.json"), true);
 
-if(in_array($_SERVER['REMOTE_ADDR'], $settings['authorized_hosts']) && isset($_REQUEST['number'])) {
-	$number = $_REQUEST['number'];
-	if(substr($_REQUEST['number'], 0, 1) != "1") {
-		$number = "1".$_REQUEST['number'];
+function do_lookup($number, $domain) {
+	if(substr($number, 0, 1) != "1") {
+		$number = "1".$number;
 	}
 	// First check if we already know about them
 	$result = do_sql($db, "SELECT v_contacts.contact_name_given, v_contacts.contact_name_family FROM v_contacts, v_contact_phones WHERE v_contact_phones.contact_uuid = v_contacts.contact_uuid AND v_contact_phones.phone_number = :number LIMIT 1", array(':number' => $number));
@@ -32,17 +31,31 @@ if(in_array($_SERVER['REMOTE_ADDR'], $settings['authorized_hosts']) && isset($_R
 		$contact_uuid = uuid();
 		do_sql($db, "INSERT INTO v_contacts(contact_uuid, domain_uuid, contact_name_given, contact_name_family) VALUES (:contact_uuid, :domain_uuid, :contact_name_given, :contact_name_family)", array(
 			':contact_uuid' => $contact_uuid,
-			':domain_uuid' => $settings['default_domain'],
+			':domain_uuid' => $domain,
 			':contact_name_given' => $fname,
 			':contact_name_family' => $lname
 		));
 		do_sql($db, "INSERT INTO v_contact_phones (contact_phone_uuid, domain_uuid, contact_uuid, phone_primary, phone_number) VALUES (:contact_phone_uuid, :domain_uuid, :contact_uuid, 1, :phone_numer)", array(
 			':contact_uuid' => $contact_uuid,
 			':contact_phone_uuid' => uuid(),
-			':domain_uuid' => $settings['default_domain'],
+			':domain_uuid' => $domain,
 			':phone_numer' => $number
 		));
 		echo $fname." ".$lname;
+	}
+}
+
+if(in_array($_SERVER['REMOTE_ADDR'], $settings['authorized_hosts'])) {
+	if(isset($_REQUEST['number'])) {
+		do_lookup($_REQUEST['number'], $settings['default_domain'])
+	} elseif(isset($_REQUEST['call'])) {
+		$fp = event_socket_create($_SESSION['event_socket_ip_address'], $_SESSION['event_socket_port'], $_SESSION['event_socket_password']);
+		if (!$fp) {
+			die("ERROR");
+		}
+		$number = trim(event_socket_request($fp, "uuid_getvar ".$_REQUEST['call']." caller_id_number"));
+		$domain = trim(event_socket_request($fp, "uuid_getvar ".$_REQUEST['call']." domain_uuid"));
+		do_lookup($number, $domain);
 	}
 } else {
 	echo "UNAUTHORIZED";
