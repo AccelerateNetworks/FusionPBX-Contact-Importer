6f91b61756c08f32f1ebcb757a84fce8222fba12
Finn <finn@callpipe.com>
2016-05-25 15:13:34 -0700

    No tailing *

--- providers/ldap.php
+++ providers/ldap.php
@@ -4,7 +4,7 @@ if(isset($settings['ldap'])) {
     global $settings;
     $ds = ldap_connect($settings['ldap']['server']);
     $r = ldap_bind($ds);
-    $query = "(|(".implode("=*$number)(", $settings['ldap']['number_fields'])."=*".$number."*))";
+    $query = "(|(".implode("=*$number)(", $settings['ldap']['number_fields'])."=*".$number."))";
     if(isset($_REQUEST['debug'])) {error_log("LDAP query: $query");}
     $sr = ldap_search($ds, $settings['ldap']['ou'], $query);
     $results = ldap_get_entries($ds, $sr);
