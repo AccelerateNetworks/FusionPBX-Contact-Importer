44c28adf48dfc1dc924393dcb8260ba344021093
Finn <finn@callpipe.com>
2016-05-25 15:06:55 -0700

    Fix LDAP query

--- providers/ldap.php
+++ providers/ldap.php
@@ -4,7 +4,7 @@ if(isset($settings['ldap'])) {
     global $settings;
     $ds = ldap_connect($settings['ldap']['server']);
     $r = ldap_bind($ds);
-    $query = "(|(".implode("=*$number*), (", $settings['ldap']['number_fields'])."=*".$number."*))";
+    $query = "(|(".implode("=*$number)(", $settings['ldap']['number_fields'])."=*".$number."*))";
     $sr = ldap_search($ds, $settings['ldap']['ou'], $query);
     $results = ldap_get_entries($ds, $sr);
     if($results['count'] > 0) {
