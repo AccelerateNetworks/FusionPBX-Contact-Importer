835775f48237163f09fa3a32c4c9e482f375c030
Finn <finn@callpipe.com>
2016-05-25 15:07:21 -0700

    Log the query

--- providers/ldap.php
+++ providers/ldap.php
@@ -5,6 +5,7 @@ if(isset($settings['ldap'])) {
     $ds = ldap_connect($settings['ldap']['server']);
     $r = ldap_bind($ds);
     $query = "(|(".implode("=*$number)(", $settings['ldap']['number_fields'])."=*".$number."*))";
+    if(isset($_REQUEST['debug'])) {error_log("LDAP query: $query");}
     $sr = ldap_search($ds, $settings['ldap']['ou'], $query);
     $results = ldap_get_entries($ds, $sr);
     if($results['count'] > 0) {
